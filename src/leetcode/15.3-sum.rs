61eb54ecbaa02b40eb8c18be42494aab3530b0f3
tpob <tpob@tpob.xyz>
2023-03-01 02:26:04 +0800

    automod again

--- src/leetcode/15.3-sum.rs
+++ src/leetcode/15.3-sum.rs
@@ -40,48 +40,6 @@ impl Solution {
     }
 }
 
-// q: is the code wrong?
-// a: yes, it is wrong, because it will return the same result multiple times.
-// q: can you fix it?
-// a: yes, I can fix it, but I don't want to fix it, because I want to learn how to fix it.
-// q: i will learn it, as long as you tell me how to fix it.
-// a: ok, I will tell you how to fix it.
-// code below
-// impl Solution {
-//     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-//         let mut nums = nums;
-//         nums.sort();
-//         let mut res = vec![];
-//         for idx in 0..nums.len() {
-//             if idx > 0 && nums[idx] == nums[idx - 1] {
-//                 continue;
-//             }
-//             let target = -nums[idx];
-//             let mut left = idx + 1;
-//             let mut right = nums.len() - 1;
-//             while left < right {
-//                 let sum = nums[left] + nums[right];
-//                 if sum < target {
-//                     left += 1;
-//                 }else if sum > target {
-//                     right -= 1;
-//                 }else {
-//                     res.push(vec![nums[idx], nums[left], nums[right]]);
-//                     while left < right && nums[left] == nums[left + 1] {
-//                         left += 1;
-//                     }
-//                     while left < right && nums[right] == nums[right - 1] {
-//                         right -= 1;
-//                     }
-//                     left += 1;
-//                     right -= 1;
-//                 }
-//             }
-//         }
-//         return res
-//     }
-// }
-
 // @lc code=end
 
 struct Solution;
--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -1,14 +1,2 @@
 #![allow(dead_code)]
-
-// automod::dir!("src/leetcode");
-#[path ="9.palindrome-number.rs"]
-mod _9_palindrome_number;
-
-#[path= "167.two-sum-ii-input-array-is-sorted.rs"]
-mod _167_two_sum_ii_input_array_is_sorted;
-
-#[path = "12.integer-to-roman.rs"]
-mod _12_integer_to_roman;
-
-#[path = "15.3-sum.rs"]
-mod _15_3_sum;
+automod::dir!("src/leetcode");
